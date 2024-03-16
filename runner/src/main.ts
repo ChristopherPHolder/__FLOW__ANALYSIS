a3e2cf246e446ad354b3012b991c95c508cdfe2a
ChristopherPHolder <toholderandrews@gmail.com>
2024-03-16 14:27:29 +0100

    feat: test s

--- runner/src/main.ts
+++ runner/src/main.ts
@@ -7,42 +7,39 @@ const ignoreApp = (app: App): boolean => {
     return [APP.SPORTSBOOK].includes(app as any);
 }
 
-console.log('Test simple file upload');
-await storeInS3('{"example": true}', 'example-name.json');
+for (const app of APPS) {
+    if (ignoreApp(app)) {
+        continue;
+    }
 
-// for (const app of APPS) {
-//     if (ignoreApp(app)) {
-//         continue;
-//     }
-//
-//     const startMobile = performance.now();
-//     const mobileReportName = 'soft-navigate-to-live-and-place-bets-' + app + '-mobile';
-//     try {
-//         const mobileReport = await navigateToLiveAndPlaceBet(app, defaultConfig);
-//         await storeInS3(mobileReportName, mobileReport);
-//         console.log(`Completed ${mobileReportName}`);
-//     }
-//     catch (e) {
-//         console.log(`Failed on ${mobileReportName} with Error:${e}`);
-//     }
-//     const endMobile = performance.now();
-//     console.log(`Collect ${app} - mobile took ${endMobile - startMobile} milliseconds`)
-//
-//     const startDesktop = performance.now();
-//     const desktopReportName = 'soft-navigate-to-live-and-place-bets-' + app + '-desktop';
-//     try {
-//         const desktopReport = await navigateToLiveAndPlaceBet(app, desktopConfig);
-//         await storeInS3(desktopReportName, desktopReport);
-//         console.log(`Completed ${desktopReportName}`);
-//     }
-//     catch (e) {
-//         console.log(`Failed on ${desktopReportName} with Error:${e}`);
-//     }
-//     const endDesktop = performance.now();
-//     console.log(`Collect ${app} - desktop took ${endDesktop - startDesktop} milliseconds`)
-//
-//     console.log(`\n --> Completed Extraction for ${app}`);
-// }
-//
-// console.log(`\n --> Completed extraction for all apps`);
+    const startMobile = performance.now();
+    const mobileReportName = 'soft-navigate-to-live-and-place-bets-' + app + '-mobile';
+    try {
+        const mobileReport = await navigateToLiveAndPlaceBet(app, defaultConfig);
+        await storeInS3(mobileReportName, mobileReport);
+        console.log(`Completed ${mobileReportName}`);
+    }
+    catch (e) {
+        console.log(`Failed on ${mobileReportName} with Error:${e}`);
+    }
+    const endMobile = performance.now();
+    console.log(`Collect ${app} - mobile took ${endMobile - startMobile} milliseconds`)
+
+    const startDesktop = performance.now();
+    const desktopReportName = 'soft-navigate-to-live-and-place-bets-' + app + '-desktop';
+    try {
+        const desktopReport = await navigateToLiveAndPlaceBet(app, desktopConfig);
+        await storeInS3(desktopReportName, desktopReport);
+        console.log(`Completed ${desktopReportName}`);
+    }
+    catch (e) {
+        console.log(`Failed on ${desktopReportName} with Error:${e}`);
+    }
+    const endDesktop = performance.now();
+    console.log(`Collect ${app} - desktop took ${endDesktop - startDesktop} milliseconds`)
+
+    console.log(`\n --> Completed Extraction for ${app}`);
+}
+
+console.log(`\n --> Completed extraction for all apps`);
 process.exit(0);
