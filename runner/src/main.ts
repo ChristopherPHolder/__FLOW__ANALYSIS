6143b6b59fd79ee8d439ab30d27d219ec741919f
ChristopherPHolder <toholderandrews@gmail.com>
2024-03-16 14:35:42 +0100

    feat: fix params

--- runner/src/main.ts
+++ runner/src/main.ts
@@ -16,7 +16,8 @@ for (const i of [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]) {
         const mobileReportName = 'soft-navigate-to-live-and-place-bets-' + app + '-mobile-' + i;
         try {
             const mobileReport = await navigateToLiveAndPlaceBet(app, defaultConfig);
-            await storeInS3(mobileReportName, mobileReport);
+
+            await storeInS3(mobileReport, mobileReportName);
             console.log(`Completed ${mobileReportName}`);
         } catch (e) {
             console.log(`Failed on ${mobileReportName} with Error:${e}`);
@@ -28,7 +29,7 @@ for (const i of [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]) {
         const desktopReportName = 'soft-navigate-to-live-and-place-bets-' + app + '-desktop-' + i;
         try {
             const desktopReport = await navigateToLiveAndPlaceBet(app, desktopConfig);
-            await storeInS3(desktopReportName, desktopReport);
+            await storeInS3(desktopReport, desktopReportName);
             console.log(`Completed ${desktopReportName}`);
         } catch (e) {
             console.log(`Failed on ${desktopReportName} with Error:${e}`);
